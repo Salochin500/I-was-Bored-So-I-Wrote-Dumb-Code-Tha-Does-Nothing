b865fe8eb5be21bd84c3bca3ab028b0afa93e103
Salochin500 <110814545+Salochin500@users.noreply.github.com>
2022-10-08 15:10:16 +1100

    Create Random.lua

---
+++ Random.lua
@@ -0,0 +1,48 @@
+local leaderstats = PlayerData = UserData = ReplicatedStorage
+local funtion("Leaderstats") = RemoteEvents
+leaderstats.UserData = RemoteEvents
+local stats = PlayerData = UserData = RemoteEvents
+local funtion("Stats") = ReplicatedStorage
+var.UserData = ReplicatedStorage 
+var.PlayerData = RemoteEvents
+
+var ReplicatedStorage
+var RemoteEvents
+
+B+i = 2
+
+local.Linear.paris.UserData
+local.script.var.script
+
+RemoteEvents = PlayerData
+ReplicatedFirst = stats
+ReplicatedFirst = leaderstats
+
+coins.leaderstats = 1
+rebiths.leaderstats = 1
+eggsopen.leaderstats = 1
+
+Linear.funtion("ColorTheme")
+ColorTheme = Color 
+ColorTheme.Brown.Green.Red.Blue.Black.White
+
+local.low.Linear.paris.PlayerData.UserData
+
+leaderstats.stats = serverscriptservice
+leaderstats.stats.serverscriptservice.RemoteEvents.ReplicatedStorage.PlayerData.UserData
+
+local leaderstats = PlayerData = UserData = ReplicatedStorage
+local funtion("Leaderstats") = ServerScriptService
+leaderstats.UserData = RemoteEvents
+local stats = PlayerData = UserData = RemoteEvents
+local funtion("Stats") = ServerScriptService
+var.UserData = ReplicatedStorage 
+var.PlayerData = RemoteEvents
+
+var.sum.array
+
+array = B+i.A+i
+
+sum = array.container
+
+var.Random.lua
